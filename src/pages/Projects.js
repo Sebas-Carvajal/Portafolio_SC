a6ef3b437a07b7ff4e3de0aa55c608768f82d7e7
Sebastian <sebastianandrescarvajallopez@gmail.com>
2025-07-26 19:59:48 -0500

    Acualizacion

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,34 +15,40 @@ function Projects() {
       </nav>
 
       <section id="Projects" class="projects-section">
-  <h1>Proyectos</h1>
+          <h1>Proyectos</h1>
 
-  <div class="project-category">
-    <h2>Python</h2>
-    <div class="project-list">
-      <div class="project-card">
-        <h3>Análisis de Datos con Pandas</h3>
-        <p>Exploración de dataset financiero con visualizaciones usando Matplotlib y Seaborn.</p>
-        <a href="#">Ver más</a>
-      </div>
-      <div class="project-card">
-        <h3>Modelo de predicción</h3>
-        <p>Modelo de regresión para predecir ventas en base a campañas de marketing.</p>
-        <a href="#">Ver más</a>
-      </div>
-    </div>
-  </div>
+          <div class="project-category">
+            <h2>Python</h2>
+            <div class="project-list">
+              <div class="project-card">
+                <h3>Análisis de Datos con Pandas</h3>
+                <p>Proyecto de análisis de datos donde se utilizó Pandas para limpiar, estructurar y 
+                  preparar un conjunto de datos relacionado con delitos cometidos en la India, 
+                  clasificados por año, tipo de crimen y estado.
+                  La información fue posteriormente visualizada en Power BI mediante dashboards interactivos
+                   que permiten filtrar dinámicamente por crimen, año o región.Este análisis facilita la 
+                   comprensión de patrones delictivos y la identificación de tendencias por estado o tipo de delito.
+                </p>
+                <a href="#">Ver más</a>
+              </div>
+              <div class="project-card">
+                <h3>Modelo de predicción</h3>
+                <p>Modelo de regresión para predecir ventas en base a campañas de marketing.</p>
+                <a href="#">Ver más</a>
+              </div>
+            </div>
+          </div>
 
-  <div class="project-category">
-    <h2>Excel</h2>
-    <div class="project-list">
-      <div class="project-card">
-        <h3>Dashboard de ventas</h3>
-        <p>Automatización de reportes con fórmulas avanzadas, gráficos y segmentación.</p>
-        <a href="#">Ver más</a>
-      </div>
-    </div>
-  </div>
+          <div class="project-category">
+            <h2>Excel</h2>
+            <div class="project-list">
+              <div class="project-card">
+                <h3>Dashboard de ventas</h3>
+                <p>Automatización de reportes con fórmulas avanzadas, gráficos y segmentación.</p>
+                <a href="#">Ver más</a>
+              </div>
+            </div>
+          </div>
 
   <div class="project-category">
     <h2>Power BI</h2>
